af11356af1f352b22d89a642d4cf4c003b8b5681
Lucas Ericsson <lucasericsson222@gmail.com>
2020-08-03 20:21:08 -0700

    Revert "Revert "Lasers for Pyratrid""
    This reverts commit 0b9effcc1bf1394f8b88a6826a92b17dfbb352cf.

--- BaseGridMoveable/Monster/Pyratrid/Pyratrid.cs
+++ BaseGridMoveable/Monster/Pyratrid/Pyratrid.cs
@@ -6,6 +6,9 @@ public class Pyratrid : Monster
 
     private Timer MoveTimer;
     private RandomNumberGenerator rng = new RandomNumberGenerator();
+
+    private PackedScene laserScene = GD.Load<PackedScene>("res://BaseGridMoveable/Monster/Pyratrid/Laser.tscn");
+    
     public override void _Ready()
     {
         rng.Randomize();
@@ -16,7 +19,14 @@ public class Pyratrid : Monster
 
     public void _on_Move_Timeout()
     {
-        switch (rng.RandiRange(1,4))
+        var randomnum = rng.RandiRange(1,4);
+        RandMove(randomnum);
+        LaserAttack(randomnum);
+    }
+
+    public void RandMove(int dir)
+    {
+        switch (dir)
         {
             case 1:
                 MoveGridRelative(1,0);
@@ -32,5 +42,54 @@ public class Pyratrid : Monster
                 break;
         }
     }
-
+    public void LaserAttack(int dir)
+    {
+        switch(dir)
+        {
+            case 1:
+                ShootLaser(false, 1);
+                break;
+            case 2:
+                ShootLaser(false, -1);
+                break;
+            case 3:
+                ShootLaser(true, 1);
+                break;
+            case 4:
+                ShootLaser(true, -1);
+                break;
+        }
+    }  
+    private void ShootLaser(bool isYDir, int pos)
+    {
+        Vector2 attackLoc = new Vector2(0,0);
+        
+        for(int i = 0; i < 20; i++)
+        {
+            if (isYDir)
+            {
+                attackLoc.x = gridPosition.x;
+                attackLoc.y = gridPosition.y + pos * (i + 1);
+            } 
+            else {
+                attackLoc.x = gridPosition.x + pos * (i + 1);
+                attackLoc.y = gridPosition.y;
+            }
+            var result = Attack((int)attackLoc.x, (int)attackLoc.y);
+            if (result.Count != 0) {
+                if (((Node2D)result["collider"]).GetNode<LightOccluder2D>("LightOccluder2D") != null)
+                {
+                    break;
+                }
+            }
+            SpawnLaser(attackLoc * 16, isYDir );
+        }
+    }
+    private void SpawnLaser(Vector2 spawnLaserPosition, bool rot)
+    {
+        var newLaser = (Area2D)laserScene.Instance();
+        newLaser.Position = spawnLaserPosition;
+        newLaser.Set("rot", rot);
+        GetParent().AddChild(newLaser);
+    }
 }
