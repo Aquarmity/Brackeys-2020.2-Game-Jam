8c91b72b0c02647994c635d5e4a81cc06d395d92
Lucas Ericsson <lucasericsson222@gmail.com>
2020-08-04 20:33:04 -0700

    Grid Display Move, and edge casses

--- Grid/Grid.cs
+++ Grid/Grid.cs
@@ -3,11 +3,20 @@ using System;
 
 public class Grid : Node2D
 {
+    [Signal]
+    public delegate void changeRooms();
     const int arraySize = 5;
+    
+    // Array 0,0 is top left corner Up: y-1 Down: y+1 Left:x-1 Right:x+1 Same as godot cordinate system
     public Room[,] roomArray = new Room[arraySize, arraySize];
     public string roomPathName = "res://Room/Rooms/";
+
+    public int currentX = 0;
+    public int currentY = 0;
+
     public override void _Ready()
     {
+        fillRoomWithBaseRoom();
         var files = getTscnInDir(roomPathName);
         foreach(string file in files)
         {
@@ -16,6 +25,17 @@ public class Grid : Node2D
         GD.Print((roomArray[0,0]));
         ShiftRowRight(0);
         GD.Print((roomArray[0,1]));
+        DisplayRoom(currentX,currentY);
+    }
+    private void fillRoomWithBaseRoom()
+    {
+        for(int i = 0; i < arraySize; i++)
+        {
+            for(int j = 0; j < arraySize; j++)
+            {
+                roomArray[i,j] = (Room)((GD.Load<PackedScene>("res://Room/Room.tscn")).Instance());
+            }
+        }
     }
 
     private void fileToRoomArr(string file)
@@ -60,6 +80,11 @@ public class Grid : Node2D
 
         }
         roomArray[row,arraySize - 1] = storage;
+        currentY -= 1;
+        if(currentY < 0)
+        {
+            currentY = arraySize - 1;
+        }
     }
     public void ShiftRowRight(int row)
     {
@@ -69,6 +94,10 @@ public class Grid : Node2D
             roomArray[row, arraySize - i] = roomArray[row, arraySize - i - 1];
         }
         roomArray[row,0] = storage;
+        currentY += 1;
+        if (currentY > arraySize - 1) {
+            currentY = 0;
+        }
     }
     public void ShiftRow(int row, bool left)
     {
@@ -90,6 +119,11 @@ public class Grid : Node2D
 
         }
         roomArray[arraySize - 1, column] = storage;
+        currentX -= 1;
+        if (currentX < 0)
+        {
+            currentX = arraySize - 1;
+        }
     }
     public void ShiftColumnDown(int column)
     {
@@ -99,6 +133,12 @@ public class Grid : Node2D
             roomArray[ arraySize - i, column] = roomArray[ arraySize - i - 1, column];
         }
         roomArray[0, column] = storage;
+
+        currentX += 1;
+        if (currentX > arraySize -1)
+        {
+            currentX = 0;
+        }
     }
     public void ShiftColumn(int column, bool up)
     {
@@ -112,4 +152,61 @@ public class Grid : Node2D
         }
     }
 
+    public void DisplayRoom(int x, int y)
+    {
+
+        var newCurrentRoom = roomArray[x, y];
+        EmitSignal(nameof(changeRooms));
+        
+        AddChild(newCurrentRoom);
+        Connect(nameof(changeRooms), newCurrentRoom, "_onChangeRooms");
+    }
+
+    public void moveRooms(int x, int y)
+    {
+        if (x > arraySize || y > arraySize) {
+            GD.Print("moveRooms: X or Y are greater than the size of the map. ");
+        }
+        if (x > arraySize - 1)
+        {
+            x = 0; // loop on x axis going over the max place
+        }
+        if (y > arraySize - 1) 
+        {
+            y = arraySize - 1; // don't loop on y axis going overboard
+        }
+        if (x < 0)
+        {
+            x = arraySize - 1; // loop on x going under the min place
+        }
+        if (y < 0)
+        {
+            y = 0; // don't loop on the y axis going over the min place
+        }
+
+        currentX = x;
+        currentY = y;
+        DisplayRoom(x,y);
+    }
+    public void moveRoomsRelative(int x, int y)
+    {
+        moveRooms(currentX + x, currentY + y);
+    }
+
+    public void moveUp()
+    {
+        moveRoomsRelative(0, -1);
+    }
+    public void moveDown()
+    {
+        moveRoomsRelative(0,1);
+    }
+    public void moveLeft()
+    {
+        moveRoomsRelative(-1, 0);
+    }
+    public void moveRight()
+    {
+        moveRoomsRelative(1,0);
+    }
 }
--- Room/Room.cs
+++ Room/Room.cs
@@ -5,6 +5,117 @@ public class Room : Node2D
 {
     [Export]
     public int[] coord = {0,0};
-
+    public int frameSymbol = 15;
     
+
+    bool left = true;
+    bool right = true;
+    bool up = true;
+    bool down = true;
+
+    public void calculateSymbol()
+    {
+        if (left)
+        {
+            if (right)
+            {
+                if (up)
+                {
+                    if (down)
+                    {
+                        frameSymbol = 15;
+                    } 
+                    else{
+                        frameSymbol = 11;
+                    }
+                }
+                else
+                {
+                    if (down)
+                    {
+                        frameSymbol = 13;
+                    } 
+                    else{
+                        frameSymbol = 6;
+                    }
+                }
+            }
+            else{
+                if (up)
+                {
+                    if (down)
+                    {
+                        frameSymbol = 14;
+                    } 
+                    else{
+                        frameSymbol = 7;
+                    }
+                }
+                else
+                {
+                    if (down)
+                    {
+                        frameSymbol = 10;
+                    } 
+                    else{
+                        frameSymbol = 4;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (right)
+            {
+                if (up)
+                {
+                    if (down)
+                    {
+                        frameSymbol = 12;
+                    } 
+                    else{
+                        frameSymbol = 8;
+                    }
+                }
+                else
+                {
+                    if (down)
+                    {
+                        frameSymbol = 9;
+                    } 
+                    else{
+                        frameSymbol = 2;
+                    }
+                }
+            }
+            else{
+                if (up)
+                {
+                    if (down)
+                    {
+                        frameSymbol = 5;
+                    } 
+                    else{
+                        frameSymbol = 1;
+                    }
+                }
+                else
+                {
+                    if (down)
+                    {
+                        frameSymbol = 3;
+                    } 
+                    else{
+                        frameSymbol = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    public void _onChangeRooms()
+    {
+        QueueFree();
+    }
+
 }
